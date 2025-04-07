4d7dcbb3f99669006f724468e2d2d989ccb9252c
EMC-MSK-Officemag <153815004+EMC-MSK-Officemag@users.noreply.github.com>
2025-04-07 18:05:04 +0300

    07042025

--- stm_quiz/src/components/GameField/GameField.jsx
+++ stm_quiz/src/components/GameField/GameField.jsx
@@ -1,20 +1,40 @@
-
 import { useState } from "react";
 
 import { arrProductSTM, name } from "../../assets/jsFiles/stmDescription.jsx";
 
-const GameField = () => {
-
-    const [productItem, setProductItem] = useState();
-    const [productList, setProductList] = useState();
-
-    return (
-        <>
-        <h1>Hi</h1>
-            <h1 onClick = {() => arrProductSTM.map((el, index) => setProductItem(<li id={index}>{el.brand}</li>))}>Hello</h1>
-            <ol onClick={() => console.log(productItem)}>{productItem}</ol>
-        </>
-    )
+const randomNumber = (min, max) => {
+    min = Math.ceil(min); // округляем до ближайшего большего целого
+    max = Math.floor(max); // округляем до ближайшего меньшего целого
+    return Math.floor(Math.random() * (max - min + 1)) + min; // генерируем случайное целое число
 }
+
+const nextChoice = (arrProductSTM) => {
+    let nextChoice = arrProductSTM[randomNumber(0, arrProductSTM.length-1)].status;
+    if (nextChoice.status) {
+        
+    }
+    console.log(arrProductSTM[randomNumber(0, arrProductSTM.length-1)].status);    
+}
+
+const GameField = () => {
+  const [productItem, setProductItem] = useState("Старт!");
+  const [productList, setProductList] = useState();
+
+  return (
+    <>
+      <h1
+        onClick={() =>
+            nextChoice(arrProductSTM)
+        //   setProductItem(
+        //     arrProductSTM.map((el, index) => <li key={index}>{el.brand}</li>)
+        //   )
+        }
+      >
+        {productItem}
+      </h1>
+      
+    </>
+  );
+};
 
 export default GameField;
